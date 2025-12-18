f6ddacbb1108f13cc0572df659138f838097a95e
shimpeisasaki <s21c1705la@s.chibakoudai.jp>
2025-12-18 21:09:31 +0900

    正規化を直した

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,7 @@ pub use crate::can::{CanInterface, CommandCounters};
 pub use crate::control::{RoboMaster, MovementCommand, LedCommand, SensorData};
 pub use crate::error::RoboMasterError;
 pub use crate::sensor::{EscData, ImuData, AttitudeData, PositionData, VelocityData, SensorState};
+#[cfg(feature = "cli")]
 pub use crate::joystick::{JoystickController, JoystickManager, ControllerInput};
 
 #[cfg(feature = "cli")]
