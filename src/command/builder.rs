a470ee98d03b87b272dd11490061e9132261d73a
shimpeisasaki <s21c1705la@s.chibakoudai.jp>
2025-12-18 18:58:28 +0900

    gain_x 256->400

--- src/command/builder.rs
+++ src/command/builder.rs
@@ -123,8 +123,8 @@ impl CommandBuilder {
         let vz_offset = if params.vz > 0.0 { deadzone } else if params.vz < 0.0 { -deadzone } else { 0.0 };
 
         // Gain adjustment: 1024.0 * (1.5 / 1.09) approx 1409.0
-        let gain_x = 256.0;
-        let gain_y = 256.0
+        let gain_x = 400.0;
+        let gain_y = 256.0;
         let gain_z = 256.0;
         let linear_x = ((gain_x * (params.vx + vx_offset) + 1024.0) as i32).clamp(0, 2047) as u16;
         let linear_y = ((gain_y * (params.vy + vy_offset) + 1024.0) as i32).clamp(0, 2047) as u16;
